1fb5fa95f5b1bddfa8f9fd0ec5f789f92ec07a6d
andreamazza89 <andrea.mazzarella89@gmail.com>
2017-07-03 08:35:43 +0100

    1.26

---
+++ chapter1/exercises/1.26.lisp
@@ -0,0 +1,24 @@
+(define (expmod base exp m)
+  (cond ((= exp 0) 1)
+        ((even? exp)
+         (remainder (* (expmod base (/ exp 2) m)
+                       (expmod base (/ exp 2) m))
+                    m))
+        (else
+         (remainder (* base (expmod base (- exp 1) m))
+                    m))))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(define (expmod base exp m)
+  (cond ((= exp 0) 1)
+        ((even? exp)
+         (remainder (square (expmod base (/ exp 2) m))
+                    m))
+        (else
+         (remainder (* base (expmod base (- exp 1) m))
+                    m))))
+
+
+In Louis' version, though the same result is achieved, `expmod` is called twice on the even-exponent branch of the recursion,
+effectively undoing the strategy of halving the exponent to compute faster.
