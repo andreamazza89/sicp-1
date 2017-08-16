dcf613b1e3dcc4364a2b1ccdec7d4b5239a19741
andreamazza89 <andrea.mazzarella89@gmail.com>
2017-08-16 08:10:31 +0100

    2.48

---
+++ chapter2/exercises/2.48.scm
@@ -0,0 +1,11 @@
+(define (make-vec x y)
+  (cons x y))
+
+(define (make-segment v1 v2)
+  (cons v1 v2))
+
+(define (start-segment segment)
+  (car segment))
+
+(define (end-segment segment)
+  (cdr segment))
