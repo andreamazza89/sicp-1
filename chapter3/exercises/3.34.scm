cf743d3eacd8fc7e9c5e39bd267b452c736ecbd7
andreamazza89 <andrea.mazzarella89@gmail.com>
2017-10-29 09:51:44 +0000

    3.34

---
+++ chapter3/exercises/3.34.scm
@@ -0,0 +1,4 @@
+; the problem is that if the product connector is set and then the multiplicand
+; connected to both inputs of the multiplier, this will cause an error when setting
+; the value on the other input unless using accurate values.
+
