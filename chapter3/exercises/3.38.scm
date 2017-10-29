19038723967f34c6ce13a3d212d5b2e6620fa1e7
andreamazza89 <andrea.mazzarella89@gmail.com>
2017-10-29 10:37:41 +0000

    3.38

---
+++ chapter3/exercises/3.38.scm
@@ -0,0 +1,17 @@
+; a
+; PePaM
+; 110 - 90 - 45
+; PeMPa
+; 110 - 55 - 35
+; PaPeM
+; 80 - 90 - 45
+; PaMPe
+; 80 - 40 - 50
+; MPaPe
+; 50 - 30 - 40
+; MPePa
+; 50 - 60 - 30
+
+; b
+; 110 (Peter reads the initial value, then the other two complete their transactions, finally Peter sets the final value to 110)
+; etc..
